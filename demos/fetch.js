d8dd99ffb3fe938841f9ca3fb38d54ba6927d1d3
scott <scottysora@gmail.com>
2024-06-03 20:08:17 -0400

    add api get examples

--- demos/fetch.js
+++ demos/fetch.js
@@ -2,9 +2,9 @@
 
 // Fetch data from the specified URL
 fetch("http://jsonplaceholder.typicode.com/users/1")
-// Parse the response as JSON
-.then((response) => response.json())
-//Log the fetched data
-.then((data) => console.log.json(data))
-// Handle any errors that occur during the process
-.catch((error) => console.error("Error:", error));
+  // Parse the response as JSON
+  .then((response) => response.json())
+  //Log the fetched data
+  .then((data) => console.log.json(data))
+  // Handle any errors that occur during the process
+  .catch((error) => console.error("Error:", error));

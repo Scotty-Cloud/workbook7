4a4a4f4d060f807868142b2b4f1e9b1605f9b039
scott <scottysora@gmail.com>
2024-06-06 08:56:03 -0400

    remove obsolete code from todo file

--- exercises/todo/scripts/todo.js
+++ exercises/todo/scripts/todo.js
@@ -21,30 +21,3 @@ fetchButton.addEventListener("click", () => {
       todoDetailsDiv.innerText = "Error fetching data.";
     });
 });
-
-// const fetchButton = document.querySelector("#fetchButton");
-// const todoIdInput = document.querySelector("#todoId");
-// const todoDetailsDiv = document.querySelector("#todoDetails");
-
-// fetchButton.addEventListener("click", () => {
-//   const todoId = todoIdInput.value;
-//   const apiUrl = `http://localhost:3000/todos/${todoId}`;
-
-//   fetch(apiUrl)
-//    .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Todo not found");
-//       }
-//       return response.json();
-//     })
-//    .then((data) => {
-//       todoDetailsDiv.innerText = `
-//         \nTitle: ${data.title}\n
-//         Completed: ${data.completed? "Yes" : "No"}\n
-//       `;
-//     })
-//    .catch((error) => {
-//       console.error("Error fetching data:", error);
-//       todoDetailsDiv.innerText = "Error fetching data.";
-//     });
-// });

14ee8cce9425fa67d635df7ba841533758653b84
scott <scottysora@gmail.com>
2024-06-06 08:55:33 -0400

    add local api to todo exercise

--- exercises/todo/scripts/todo.js
+++ exercises/todo/scripts/todo.js
@@ -5,14 +5,15 @@ const todoDetailsDiv = document.querySelector("#todoDetails");
 fetchButton.addEventListener("click", () => {
   const todoId = todoIdInput.value;
 
-  const apiUrl = `https://jsonplaceholder.typicode.com/todos/${todoId}`;
+  // const apiUrl = `https://jsonplaceholder.typicode.com/todos/${todoId}`;
+  const apiUrl = `http://localhost:3000/todos/${todoId}`;
 
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
       todoDetailsDiv.innerText = `
         \nTitle: ${data.title}\n
-        Completed: ${data.completed ? "Yes" : "No"}\n
+        Completed: ${data.completed ? "True" : "False"}\n
       `;
     })
     .catch((error) => {
@@ -20,3 +21,30 @@ fetchButton.addEventListener("click", () => {
       todoDetailsDiv.innerText = "Error fetching data.";
     });
 });
+
+// const fetchButton = document.querySelector("#fetchButton");
+// const todoIdInput = document.querySelector("#todoId");
+// const todoDetailsDiv = document.querySelector("#todoDetails");
+
+// fetchButton.addEventListener("click", () => {
+//   const todoId = todoIdInput.value;
+//   const apiUrl = `http://localhost:3000/todos/${todoId}`;
+
+//   fetch(apiUrl)
+//    .then((response) => {
+//       if (!response.ok) {
+//         throw new Error("Todo not found");
+//       }
+//       return response.json();
+//     })
+//    .then((data) => {
+//       todoDetailsDiv.innerText = `
+//         \nTitle: ${data.title}\n
+//         Completed: ${data.completed? "Yes" : "No"}\n
+//       `;
+//     })
+//    .catch((error) => {
+//       console.error("Error fetching data:", error);
+//       todoDetailsDiv.innerText = "Error fetching data.";
+//     });
+// });
